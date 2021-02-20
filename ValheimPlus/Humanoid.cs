08920f67493059721258121e704ebe4074919e1d
GaelicGamer <noah.castro.hart@gmail.com>
2021-02-19 20:56:45 -0500

    Unarmed Damage Scaling
    Adding configurable value for scaling unarmed damage based on how high your skill is with it.

---
+++ ValheimPlus/Humanoid.cs
@@ -0,0 +1,32 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace ValheimPlus
+{
+    [HarmonyPatch(typeof(Humanoid), "GetCurrentWeapon")]
+    public static class ModifyCurrentWeapon
+    {
+        private static ItemDrop.ItemData Postfix(ItemDrop.ItemData __weapon, ref Character __instance)
+        {
+            if (__weapon != null) {
+                #if DEBUG
+                Debug.Log("Attempting to update Unarmed Damage");
+                #endif
+                if (__weapon.m_shared.m_name == "Unarmed")
+                {
+                    __weapon.m_shared.m_damages.m_blunt = __instance.GetSkillFactor(Skills.SkillType.Unarmed) * Settings.getFloat("Unarmed", "baseDamage"); ;
+                    
+                    #if DEBUG
+                    Debug.Log("Updated Unarmed Damage to :" + __weapon.m_shared.m_damages.m_blunt);
+                    #endif
+                }
+            }
+
+            return __weapon;
+        }
+    }
+}
