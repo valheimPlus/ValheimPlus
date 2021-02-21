4917623417d0666e5aacd55a4fc7e50f4705fa05
radmint <pdbystrom@gmail.com>
2021-02-20 23:27:26 -0800

    adding functionality to remove death pin on map after tombstone is recovered

---
+++ ValheimPlus/TombStone.cs
@@ -0,0 +1,30 @@
+﻿using HarmonyLib;
+using UnityEngine;
+using ValheimPlus.Configurations;
+
+namespace ValheimPlus
+{
+    [HarmonyPatch(typeof(TombStone), "OnTakeAllSuccess")]
+    public static class RemoveDeathPinOnTombStoneEmpty
+    {
+        private static void Postfix()
+        {
+            if (Configuration.Current.Map.IsEnabled && Configuration.Current.Map.RemoveDeathPinOnTombstoneEmpty)
+            {
+                PlayerProfile playerProfile = Game.instance.m_playerProfile;
+                long worldUID = ZNet.instance.GetWorldUID();
+
+                if(!playerProfile.m_worldData.TryGetValue(worldUID, out PlayerProfile.WorldPlayerData worldData))
+                {
+                    return;
+                }
+
+                // resets death point data to remove death pin when removing all things from tombstone
+                worldData.m_deathPoint = Vector3.zero;
+                worldData.m_haveDeathPoint = false;
+
+                playerProfile.m_worldData[worldUID] = worldData;
+            }
+        }
+    }
+}
