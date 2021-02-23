a15c9c6bb200e99caabbcbcd157298e2e517e7ed
Zogniton <Zogniton@gmail.com>
2021-02-23 23:41:39 +0200

    Larger inventory support * Can extend player invetory up to 7 x 8 from 4 x 8   * I could not add a scrollbar to the player inventory, 7 rows is the maximum you can fit while still seeing a chest inventory using 115% UI scale. * Can extend chest inventory (wood up to 10 x 8, iron up to 20 x 8)   * Chests have a scrollbar if larger than 4 x 8

--- ValheimPlus/Inventory.cs
+++ ValheimPlus/Inventory.cs
@@ -1,5 +1,6 @@
 ﻿using HarmonyLib;
 using System;
+using UnityEngine;
 using ValheimPlus.Configurations;
 
 namespace ValheimPlus
@@ -14,6 +15,58 @@ namespace ValheimPlus
                 if (Configuration.Current.Items.noTeleportPrevention)
                     __result = true;
             }
+        }
+    }
+
+    [HarmonyPatch(typeof(Inventory), MethodType.Constructor)]
+    [HarmonyPatch(new Type[] { typeof(string), typeof(Sprite), typeof(int), typeof(int) })]
+    public static class changeInventorySlotCount
+    {
+        static void Prefix(ref string name, ref Sprite bkg, ref int w, ref int h)
+        {
+            if (Configuration.Current.Inventory.IsEnabled)
+            {
+                // Wood chest
+                if (h == 2 && w == 5)
+                {
+                    w = Configuration.Current.Inventory.woodChestColumns > 8 ? 8 : Configuration.Current.Inventory.woodChestColumns;
+                    h = Configuration.Current.Inventory.woodChestRows > 10 ? 10 : Configuration.Current.Inventory.woodChestRows;
+                }
+                // Player inventory
+                else if (h == 4 && w == 8)
+                {
+                    h = Configuration.Current.Inventory.playerInventoryRows > 7 ? 7 : Configuration.Current.Inventory.playerInventoryRows;
+                }
+                // Iron chest, cart, boat
+                else if (h == 3 && w == 6)
+                {
+                    w = Configuration.Current.Inventory.ironChestColumns > 8 ? 8 : Configuration.Current.Inventory.ironChestColumns;
+                    h = Configuration.Current.Inventory.ironChestRows > 10 ? 10 : Configuration.Current.Inventory.ironChestColumns;
+                }
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(InventoryGui), "Show")]
+    public class InventoryGuiAwake
+    {
+        public static void Postfix(ref InventoryGui __instance)
+        {
+            RectTransform container = __instance.m_container;
+            RectTransform player = __instance.m_player;
+
+            int playerInventoryRows = Configuration.Current.Inventory.playerInventoryRows > 7 ? 7 : Configuration.Current.Inventory.playerInventoryRows;
+
+            float oneRowSize = 70.5f;
+            float containerOriginalY = -90.0f;
+            float containerHeight = -340.0f;
+
+            float containerNewY = containerOriginalY - oneRowSize * playerInventoryRows;
+
+            player.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, playerInventoryRows * oneRowSize);
+
+            container.offsetMax = new Vector2(610, containerNewY);
+            container.offsetMin = new Vector2(40, containerNewY + containerHeight);
         }
     }
 }
