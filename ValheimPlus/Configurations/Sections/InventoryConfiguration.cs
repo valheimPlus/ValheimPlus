3ad13977b907fd5aa2dbac2fc10dcf88d15d58f2
Zogniton <Zogniton@gmail.com>
2021-02-23 23:41:39 +0200

    Larger inventory support * Can extend player invetory up to 7 x 8 from 4 x 8   * I could not add a scrollbar to the player inventory, 7 rows is the maximum you can fit while still seeing a chest inventory using 115% UI scale. * Can extend chest inventory (wood up to 10 x 8, iron up to 20 x 8)   * Chests have a scrollbar if larger than 4 x 8

---
+++ ValheimPlus/Configurations/Sections/InventoryConfiguration.cs
@@ -0,0 +1,12 @@
+﻿namespace ValheimPlus.Configurations.Sections
+{
+    public class InventoryConfiguration : ServerSyncConfig<InventoryConfiguration>
+    {
+        public int playerInventoryRows { get; set; } = 4;
+        public int woodChestColumns { get; set; } = 5;
+        public int woodChestRows { get; set; } = 2;
+        public int ironChestColumns { get; set; } = 8;
+        public int ironChestRows { get; set; } = 3;
+    }
+
+}
