3899da0743e305dae7fca9b57dd69d87170b7f97
Bruno Vasconcelos <bruno.vasconcelos@auspex.com.br>
2021-02-23 09:32:16 -0300

    instructed code on how to be less messy

--- ValheimPlus/Smelter.cs
+++ ValheimPlus/Smelter.cs
@@ -44,32 +44,15 @@ namespace ValheimPlus
     {
         private static bool Prefix(string ore,int stack, ref Smelter __instance) 
         {
+            var smelter = __instance; //allowing access to local function
 
             if (__instance.gameObject.name.Contains("kiln")) 
             {
                 if (Configuration.Current.Kiln.IsEnabled) 
                 {
                     if (Configuration.Current.Kiln.autoDeposit) {
-                        Collider[] hitColliders = hitColliders = Physics.OverlapSphere(__instance.gameObject.transform.localPosition, Configuration.Current.Furnace.autoDepositRange);
-                        foreach (var hitCollider in hitColliders) {
-                            if (hitCollider.gameObject.GetComponentInParent<Container>() != null) {
-                                GameObject itemPrefab = ObjectDB.instance.GetItemPrefab(__instance.GetItemConversion(ore).m_to.gameObject.name);
-                                ZNetView.m_forceDisableInit = true;
-                                GameObject spawnedOre = UnityEngine.Object.Instantiate<GameObject>(itemPrefab);
-                                ZNetView.m_forceDisableInit = false;
-                                ItemDrop comp = spawnedOre.GetComponent<ItemDrop>();
-                                comp.m_itemData.m_stack = stack;
-                                var result = hitCollider.gameObject.GetComponentInParent<Container>().m_inventory.AddItem(comp.m_itemData);
-                                if(!result) 
-                                {
-                                    continue;
-                                }
-                                __instance.m_produceEffects.Create(__instance.transform.position, __instance.transform.rotation, null, 1f);
-                                UnityEngine.Object.Destroy(spawnedOre);
-                                return false;
-                            }
-                        }
-
+                        var result = spawn(true);
+                        return result; 
                     }
                 }
             }
@@ -77,27 +60,49 @@ namespace ValheimPlus
             {
                 if (Configuration.Current.Furnace.IsEnabled) {
                     if (Configuration.Current.Furnace.autoDeposit) {
-                        Collider[] hitColliders = hitColliders = Physics.OverlapSphere(__instance.gameObject.transform.localPosition, Configuration.Current.Furnace.autoDepositRange);
-                        foreach (var hitCollider in hitColliders) {
-                            if (hitCollider.gameObject.GetComponentInParent<Container>() != null) {
-                                GameObject itemPrefab = ObjectDB.instance.GetItemPrefab(__instance.GetItemConversion(ore).m_to.gameObject.name);
-                                ZNetView.m_forceDisableInit = true;
-                                GameObject spawnedOre = UnityEngine.Object.Instantiate<GameObject>(itemPrefab);
-                                ZNetView.m_forceDisableInit = false;
-                                ItemDrop comp = spawnedOre.GetComponent<ItemDrop>();
-                                comp.m_itemData.m_stack = stack;
-                                var result = hitCollider.gameObject.GetComponentInParent<Container>().m_inventory.AddItem(comp.m_itemData);
-                                if (!result) {
-                                    continue;
-                                }
-                                __instance.m_produceEffects.Create(__instance.transform.position, __instance.transform.rotation, null, 1f);
-                                UnityEngine.Object.Destroy(spawnedOre);
-                                return false;
-                            }
-                        }
+                        var result = spawn(false);
+                        return result;
                     }
                 }
             }
+
+            bool spawn(bool isKiln) {
+
+                //SphereCast grabbing all overlaps (didn't bother trying to find a mask, so this might be "heavy")
+                Collider[] hitColliders = Physics.OverlapSphere(smelter.gameObject.transform.localPosition, isKiln ? Configuration.Current.Kiln.autoDepositRange : Configuration.Current.Furnace.autoDepositRange);
+                
+                foreach (var hitCollider in hitColliders) {
+                    //Search for Containers components
+                    if (hitCollider.gameObject.GetComponentInParent<Container>() != null) {
+
+                        //Replicating original code, just "spawning/adding" the item inside the chest makes it "not have a prefab"
+                        GameObject itemPrefab = ObjectDB.instance.GetItemPrefab(smelter.GetItemConversion(ore).m_to.gameObject.name);
+
+                        //Also replication of original code, really have no idead what it is for, didn't bother look
+                        ZNetView.m_forceDisableInit = true;
+                        GameObject spawnedOre = UnityEngine.Object.Instantiate<GameObject>(itemPrefab);
+                        ZNetView.m_forceDisableInit = false;
+
+                        //assign stack size, nobody wants a 0/20 stack of metals (its not very usefull)
+                        ItemDrop comp = spawnedOre.GetComponent<ItemDrop>();
+                        comp.m_itemData.m_stack = stack;
+
+                        var result = hitCollider.gameObject.GetComponentInParent<Container>().m_inventory.AddItem(comp.m_itemData);
+                        if (!result) {
+                            //Chest full, move to the next
+                            continue;
+                        }
+                        
+                        smelter.m_produceEffects.Create(smelter.transform.position, smelter.transform.rotation, null, 1f);
+                        UnityEngine.Object.Destroy(spawnedOre);
+
+                        return false;
+                    }
+                }
+
+                return true;
+            }
+
             return true;
         }
 
