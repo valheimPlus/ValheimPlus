6709e8b812c9c3f84de30891236706016a62e50c
Robert Roos <robert@iodesk.net>
2021-02-23 14:17:53 +0100

    Starting work on config manager/reader

---
+++ ValheimPlusManager/ValheimPlusManager/Models/ValheimPlusConf.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ValheimPlusManager.Models
+{
+    public class ValheimPlusConf
+    {
+        public bool PlayerEnabled { get; set; } 
+    }
+}
---
+++ ValheimPlusManager/ValheimPlusManager/SupportClasses/ConfigManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using IniParser;
+using IniParser.Model;
+
+namespace ValheimPlusManager.SupportClasses
+{
+    public class ConfigManager
+    {
+        public void ReadConfigFile()
+        {
+            var parser = new FileIniDataParser();
+        }
+    }
+}
