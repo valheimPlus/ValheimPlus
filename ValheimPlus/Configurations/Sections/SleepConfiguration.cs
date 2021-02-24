8c12e4d37df95fae528117c033085d5447cee5fa
Noah Hart <noah.castro.hart@gmail.com>
2021-02-23 20:41:38 -0500

    Update config with Sleep Parameters

--- ValheimPlus/Configurations/Sections/SleepConfiguration.cs
+++ ValheimPlus/Configurations/Sections/SleepConfiguration.cs
@@ -7,8 +7,8 @@ namespace ValheimPlus.Configurations.Sections
 {
     public class SleepConfiguration : ServerSyncConfig<SleepConfiguration>
     {
-        public int numberOfPlayersToSleep { get; internal set; } = 1;
         public bool byPlayers { get; internal set; } = false;
+        public int numberOfPlayersToSleep { get; internal set; } = 1;
         public bool byPercentage { get; internal set; } = false;
         public double percentageOfPlayersToSleep { get; internal set; } = 50;
     }
