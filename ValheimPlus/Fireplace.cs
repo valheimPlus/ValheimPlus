97e0370b2a89a3b1bc2218d9a7612640d009e229
vincentlau36 <vincentlau36@gmail.com>
2021-02-24 19:41:12 -0800

    Correct spacing

--- ValheimPlus/Fireplace.cs
+++ ValheimPlus/Fireplace.cs
@@ -9,24 +9,23 @@ namespace ValheimPlus
         [HarmonyPatch(typeof(Fireplace), "UpdateFireplace")]
         public static class TorchesNoFuel 
         {
-            private static void Postfix(Fireplace __instance, ref ZNetView ___m_nview) 
-            {
-                if (Configuration.Current.Fireplace.IsEnabled)
-		{
-			if (Configuration.Current.Fireplace.onlyTorches)
-	                {
-		                if (__instance.GetHoverText().Contains("torch") || __instance.GetHoverText().Contains("Sconce") || __instance.name.Contains("brazier")) 
-                        	{
-                            		___m_nview.GetZDO().Set("fuel", 1f);
-                        	}  
-	                } 
-			else
-                    	{
-                        	___m_nview.GetZDO().Set("fuel", 3f);
-                    	}
-	            }
-
-            }
+			private static void Postfix(Fireplace __instance, ref ZNetView ___m_nview) 
+			{
+				if (Configuration.Current.Fireplace.IsEnabled)
+				{
+					if (Configuration.Current.Fireplace.onlyTorches)
+					{
+						if (__instance.GetHoverText().Contains("torch") || __instance.GetHoverText().Contains("Sconce") || __instance.name.Contains("brazier")) 
+						{
+							___m_nview.GetZDO().Set("fuel", 1f);
+						}  
+					} 
+					else
+					{
+						___m_nview.GetZDO().Set("fuel", 3f);
+					}
+				}
+			}
         }
     }
 }
