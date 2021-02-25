2bd59168c72195dff1a466e5559b05a250b714ee
Algorithman <algorithman.chris@googlemail.com>
2021-02-25 18:11:24 +0100

    Plants under roof and in all biomes

---
+++ ValheimPlus/Configurations/Sections/PlantConfiguration.cs
@@ -0,0 +1,20 @@
+﻿// ---------------------------------------------------------------------------------------
+// Solution: ValheimPlus
+// Project: ValheimPlus
+// Filename: PlantConfiguration.cs
+// 
+// Last modified: 2021-02-25 17:36
+// Created:       2021-02-25 17:36
+// 
+// Copyright: 2021 Walter Wissing & Co
+// ---------------------------------------------------------------------------------------
+
+namespace ValheimPlus.Configurations.Sections
+{
+    public class PlantConfiguration : ServerSyncConfig<PlantConfiguration>
+    {
+        public bool noWrongBiome { get; set; } = false;
+
+        public bool growWithoutSun { get; set; } = false;
+    }
+}
---
+++ ValheimPlus/Plant.cs
@@ -0,0 +1,45 @@
+﻿// ---------------------------------------------------------------------------------------
+// Solution: ValheimPlus
+// Project: ValheimPlus
+// Filename: Plant.cs
+// 
+// Last modified: 2021-02-25 17:34
+// Created:       2021-02-25 17:34
+// 
+// Copyright: 2021 Walter Wissing & Co
+// ---------------------------------------------------------------------------------------
+
+using System;
+using HarmonyLib;
+using UnityEngine.UIElements;
+using ValheimPlus.Configurations;
+
+namespace ValheimPlus
+{
+    [HarmonyPatch(typeof(Plant), "UpdateHealth")]
+    public static class PlantBiomeFix
+    {
+
+        public static void Postfix(ref Plant __instance)
+        {
+            if (Configuration.Current.Plant.IsEnabled)
+            {
+                if (Configuration.Current.Plant.noWrongBiome)
+                {
+                    if (__instance.m_status == Plant.Status.WrongBiome)
+                    {
+                        __instance.m_status = Plant.Status.Healthy;
+                    }
+                }
+
+                if (Configuration.Current.Plant.growWithoutSun)
+                {
+                    if (__instance.m_status == Plant.Status.NoSun)
+                    {
+                        __instance.m_status = Plant.Status.Healthy;
+                    }
+                }
+            }
+        }
+    }
+}
