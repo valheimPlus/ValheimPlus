cc85343edcd697d27ba7174281d142626ebdaf71
Jules <github@sirskunkalot.de>
2021-02-26 00:12:21 +0100

    correct comments

--- resources/Unix/start_server_bepinex.sh
+++ resources/Unix/start_server_bepinex.sh
@@ -15,7 +15,7 @@ executable_name="valheim_server.x86_64"
 
 # EDIT THIS: Valheim server parameters
 # Can be overriden by script parameters named exactly like the ones for the Valheim executable
-# (e.g. ./run_bepinex.sh -name="MyValheimPlusServer" -password="somethingsafe" -port=2456 -world="myworld")
+# (e.g. ./start_server_bepinex.sh -name="MyValheimPlusServer" -password="somethingsafe" -port=2456 -world="myworld")
 server_name="Valheim+"
 server_password="password"
 server_port=2456
@@ -36,7 +36,7 @@ export DOORSTOP_CORLIB_OVERRIDE_PATH=./unstripped_corlib
 # ----- (unless you know what you're doing) ------
 
 if [ ! -x "$1" -a ! -x "$executable_name" ]; then
-    echo "Please open run_bepinex.sh in a text editor and provide the correct executable."
+    echo "Please open start_server_bepinex.sh in a text editor and provide the correct executable."
     exit 1
 fi
 
