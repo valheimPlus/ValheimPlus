5ff44a6ac54823eb4ade67be06e6974e2685092c
vincentlau36 <vincentlau36@gmail.com>
2021-02-25 22:05:52 -0800

    Intercept and change skill lower factor
    Added configuration (server synced) for Skills.
    Added ability to change the skill lower factor on player death.

---
+++ ValheimPlus/Configurations/Sections/SkillsConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ValheimPlus.Configurations.Sections
+{
+    public class SkillsConfiguration : ServerSyncConfig<SkillsConfiguration>
+    {
+        public float skillDeathLowerFactor { get; internal set; }
+    }
+}
---
+++ ValheimPlus/Skills.cs
@@ -0,0 +1,22 @@
+﻿using HarmonyLib;
+using ValheimPlus.Configurations;
+using UnityEngine;
+
+namespace ValheimPlus
+{
+    class SkillsModification
+    {
+        [HarmonyPatch(typeof(Skills), "LowerAllSkills")]
+        public static class ChangeSkillLossOnDeath
+        {
+            private static void Prefix(ref float factor)
+            {
+                if (Configuration.Current.Skills.IsEnabled)
+                {
+                    float decreaseFactor = Configuration.Current.Skills.skillDeathLowerFactor;
+                    factor = decreaseFactor;
+                }
+            }
+        }
+    }
+}
