65be6cad6ff614a7312c37c4e2486828c51179b4
Kevin nX_ J <contact@n-x.xyz>
2021-02-26 12:56:14 +0100

    Minor adjustments to ItemDrop.cs
    - Simplified ItemStak and ItemWeight modifiers, now allow for +/- values
    
    - Updated to 0.9.2 in terms of version

--- ValheimPlus/GameClasses/ItemDrop.cs
+++ ValheimPlus/GameClasses/ItemDrop.cs
@@ -20,25 +20,14 @@ namespace ValheimPlus
 
             if (Configuration.Current.Items.IsEnabled)
             {
-                // 50 results in a float point error
-                float itemWeigthReduction = (Configuration.Current.Items.baseItemWeightReduction == 50 ? 51 : Configuration.Current.Items.baseItemWeightReduction);
-                itemWeigthReduction = (Configuration.Current.Items.baseItemWeightReduction == -50 ? -51 : Configuration.Current.Items.baseItemWeightReduction);
+                
+                __instance.m_itemData.m_shared.m_weight = Helper.applyModifierValue(__instance.m_itemData.m_shared.m_weight, Configuration.Current.Items.baseItemWeightReduction);
 
-                if (itemWeigthReduction > 0)
-                {
-                    __instance.m_itemData.m_shared.m_weight = (float)(__instance.m_itemData.m_shared.m_weight + ((__instance.m_itemData.m_shared.m_weight / 100) * itemWeigthReduction));
-                }
-                if (itemWeigthReduction < 0)
-                {
-                    __instance.m_itemData.m_shared.m_weight = (float)(__instance.m_itemData.m_shared.m_weight - ((__instance.m_itemData.m_shared.m_weight / 100) * (itemWeigthReduction * -1)));
-                }
-
-                float itemStackMultiplier = Configuration.Current.Items.itemStackMultiplier;
                 if (__instance.m_itemData.m_shared.m_maxStackSize > 1)
                 {
-                    if (itemStackMultiplier >= 1)
+                    if (Configuration.Current.Items.itemStackMultiplier >= 1)
                     {
-                        __instance.m_itemData.m_shared.m_maxStackSize = __instance.m_itemData.m_shared.m_maxStackSize + (int)(((float)(__instance.m_itemData.m_shared.m_maxStackSize) / 100) * itemStackMultiplier);
+                        __instance.m_itemData.m_shared.m_maxStackSize = (int)Helper.applyModifierValue(__instance.m_itemData.m_shared.m_maxStackSize, Configuration.Current.Items.itemStackMultiplier);
                     }
                 }
             }
