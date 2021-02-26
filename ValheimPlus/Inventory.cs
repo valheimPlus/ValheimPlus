b7aa91c8d8cc2c8279c1060160936ae74c57d823
Miguel <miguel.terol.93@gmail.com>
2021-02-26 13:29:50 +0100

    Improve code of Inventory
    Change to Prefix, if config is enabled and noTeleport set, we can just ignore the function so return false

--- ValheimPlus/Inventory.cs
+++ ValheimPlus/Inventory.cs
@@ -7,13 +7,17 @@ namespace ValheimPlus
     [HarmonyPatch(typeof(Inventory), "IsTeleportable")]
     public static class noItemTeleportPrevention
     {
-        private static void Postfix(ref Boolean __result)
+        private static void Prefix(ref Boolean __result)
         {
             if (Configuration.Current.Items.IsEnabled)
             {
-                if (Configuration.Current.Items.noTeleportPrevention)
+                if (Configuration.Current.Items.noTeleportPrevention) 
+                {
                     __result = true;
-            }
+                    return false; // Stop any unnecessary calcs by interrupting the normal func
+                }
+            } 
+            return true; // continue as normal
         }
     }
 }
