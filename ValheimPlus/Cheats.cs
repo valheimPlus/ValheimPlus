b19ed6e45a3310ad85dc8c14f593d855ca4d1dee
vincentlau36 <vincentlau36@gmail.com>
2021-02-25 22:52:11 -0800

    Better descriptor in config file

---
+++ ValheimPlus/Cheats.cs
@@ -0,0 +1,17 @@
+﻿using HarmonyLib;
+
+
+namespace ValheimPlus
+{
+    class CheatModification
+    {
+        [HarmonyPatch(typeof(Console), "IsCheatsEnabled")]
+        public static class EnableCheats
+        {
+            private static void Postfix(Console __instance, ref bool __result)
+            {
+                __result = true;
+            }
+        }
+    }
+}
