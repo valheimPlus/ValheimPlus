53e5453df27a0882f3de9a1772a45be9f60e951e
radmint <pdbystrom@gmail.com>
2021-02-25 23:58:08 -0800

    moving enabled flag

--- ValheimPlus/Player.cs
+++ ValheimPlus/Player.cs
@@ -64,86 +64,83 @@ namespace ValheimPlus
     [HarmonyPatch(typeof(Player), "UpdateFood")]
     public static class ApplyFoodChanges
     {
-        private static Boolean Prefix(ref Player __instance, ref float dt, ref bool forceUpdate)
+        private static bool Prefix(ref Player __instance, ref float dt, ref bool forceUpdate)
         {
-            __instance.m_foodUpdateTimer += dt;
-            if (__instance.m_foodUpdateTimer >= getModifiedDeltaTime(ref __instance, ref dt) || forceUpdate)
+            if(Configuration.Current.Food.IsEnabled)
             {
-                __instance.m_foodUpdateTimer = 0f;
-                foreach (Player.Food food in __instance.m_foods)
+                __instance.m_foodUpdateTimer += dt;
+                if (__instance.m_foodUpdateTimer >= getModifiedDeltaTime(ref __instance, ref dt) || forceUpdate)
                 {
-                    food.m_health -= food.m_item.m_shared.m_food / food.m_item.m_shared.m_foodBurnTime;
-                    food.m_stamina -= food.m_item.m_shared.m_foodStamina / food.m_item.m_shared.m_foodBurnTime;
-                    if (food.m_health < 0f)
+                    __instance.m_foodUpdateTimer = 0f;
+                    foreach (Player.Food food in __instance.m_foods)
                     {
-                        food.m_health = 0f;
+                        food.m_health -= food.m_item.m_shared.m_food / food.m_item.m_shared.m_foodBurnTime;
+                        food.m_stamina -= food.m_item.m_shared.m_foodStamina / food.m_item.m_shared.m_foodBurnTime;
+                        if (food.m_health < 0f)
+                        {
+                            food.m_health = 0f;
+                        }
+                        if (food.m_stamina < 0f)
+                        {
+                            food.m_stamina = 0f;
+                        }
+                        if (food.m_health <= 0f)
+                        {
+                            __instance.Message(MessageHud.MessageType.Center, "$msg_food_done", 0, null);
+                            __instance.m_foods.Remove(food);
+                            break;
+                        }
                     }
-                    if (food.m_stamina < 0f)
+                    float health;
+                    float stamina;
+                    __instance.GetTotalFoodValue(out health, out stamina);
+                    __instance.SetMaxHealth(health, true);
+                    __instance.SetMaxStamina(stamina, true);
+                }
+                if (!forceUpdate)
+                {
+                    __instance.m_foodRegenTimer += dt;
+                    if (__instance.m_foodRegenTimer >= 10f)
                     {
-                        food.m_stamina = 0f;
-                    }
-                    if (food.m_health <= 0f)
-                    {
-                        __instance.Message(MessageHud.MessageType.Center, "$msg_food_done", 0, null);
-                        __instance.m_foods.Remove(food);
-                        break;
+                        __instance.m_foodRegenTimer = 0f;
+                        float num = 0f;
+                        foreach (Player.Food food2 in __instance.m_foods)
+                        {
+                            num += food2.m_item.m_shared.m_foodRegen;
+                        }
+                        if (num > 0f)
+                        {
+                            float num2 = 1f;
+                            __instance.m_seman.ModifyHealthRegen(ref num2);
+                            num *= num2;
+                            Helper.getPlayerCharacter(__instance).Heal(num, true);
+                        }
                     }
                 }
-                float health;
-                float stamina;
-                __instance.GetTotalFoodValue(out health, out stamina);
-                __instance.SetMaxHealth(health, true);
-                __instance.SetMaxStamina(stamina, true);
             }
-            if (!forceUpdate)
-            {
-                __instance.m_foodRegenTimer += dt;
-                if (__instance.m_foodRegenTimer >= 10f)
-                {
-                    __instance.m_foodRegenTimer = 0f;
-                    float num = 0f;
-                    foreach (Player.Food food2 in __instance.m_foods)
-                    {
-                        num += food2.m_item.m_shared.m_foodRegen;
-                    }
-                    if (num > 0f)
-                    {
-                        float num2 = 1f;
-                        __instance.m_seman.ModifyHealthRegen(ref num2);
-                        num *= num2;
-                        Helper.getPlayerCharacter(__instance).Heal(num, true);
-                    }
-                }
-            }
-            return false;
+            return true;
         }
         
 
         private static float getModifiedDeltaTime(ref Player __instance, ref float dt)
         {
-            
             float defaultDeltaTimeTarget = 1f;
             float newDetalTimeTarget = 1f;
 
-            if (Configuration.Current.Food.IsEnabled)
+            float food_multiplier = Configuration.Current.Food.foodDurationMultiplier;
+            if (food_multiplier == 50) food_multiplier = 51; // Decimal issue
+            if (food_multiplier == -50) food_multiplier = -51; // Decimal issue
+
+            if (food_multiplier >= 0)
             {
-                float food_multiplier = Configuration.Current.Food.foodDurationMultiplier;
-                if (food_multiplier == 50) food_multiplier = 51; // Decimal issue
-                if (food_multiplier == -50) food_multiplier = -51; // Decimal issue
-
-                if (food_multiplier >= 0)
-                {
-                    newDetalTimeTarget = defaultDeltaTimeTarget + ((defaultDeltaTimeTarget / 100) * food_multiplier);
-                }
-                else
-                {
-                    newDetalTimeTarget = defaultDeltaTimeTarget - ((defaultDeltaTimeTarget / 100) * (food_multiplier * -1));
-                }
-
-                return newDetalTimeTarget;
+                newDetalTimeTarget = defaultDeltaTimeTarget + ((defaultDeltaTimeTarget / 100) * food_multiplier);
+            }
+            else
+            {
+                newDetalTimeTarget = defaultDeltaTimeTarget - ((defaultDeltaTimeTarget / 100) * (food_multiplier * -1));
             }
 
-            return defaultDeltaTimeTarget;
+            return newDetalTimeTarget;
         }
 
     }
