ba664efdeceb0e16be2ff657354000d20529484f
Algorithman <algorithman.chris@googlemail.com>
2021-02-26 11:22:53 +0100

    First step for ingame config settings change / console command interface

---
+++ ValheimPlus/ConsoleCommands/BaseConsoleCommand.cs
@@ -0,0 +1,81 @@
+﻿// ValheimPlus
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace ValheimPlus.ConsoleCommands
+{
+    public class BaseConsoleCommand
+    {
+        public string HelpText { get; internal set; }
+        public string CommandName { get; internal set; }
+
+        public virtual bool ParseCommand(string input)
+        {
+            return false;
+        }
+
+        public bool AdminOnly { get; internal set; }
+        
+        private static List<BaseConsoleCommand> consoleCommandInstances = new List<BaseConsoleCommand>();
+
+        public static void InitializeCommand<T>() where T : BaseConsoleCommand, new()
+        {
+            if (!consoleCommandInstances.Any(x => x is T))
+            {
+                consoleCommandInstances.Add(new T());
+            }
+        }
+
+
+        public static bool TryExecuteCommand(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                return false;
+            }
+
+            string[] split = input.Split(' ');
+            BaseConsoleCommand command = consoleCommandInstances.FirstOrDefault(x => String.Equals(x.CommandName, split[0], StringComparison.CurrentCultureIgnoreCase));
+            
+            if (command != null)
+            {
+                return command.ParseCommand(input);
+            }
+
+            return false;
+        }
+
+        protected static float GetFloat(string input)
+        {
+            if (!float.TryParse(input, out var val))
+            {
+                return float.NaN;
+            }
+
+            return val;
+        }
+
+        protected static int GetInt(string input)
+        {
+            if (!int.TryParse(input, out var val))
+            {
+                return int.MinValue;
+            }
+
+            return val;
+        }
+
+        protected static KeyCode GetKeyCode(string input)
+        {
+            if (System.Enum.TryParse<KeyCode>(input, out var result))
+            {
+                return result;
+            }
+
+            return KeyCode.None;
+        }
+    }
+}
---
+++ ValheimPlus/ConsoleCommands/SetConfigurationValue.cs
@@ -0,0 +1,98 @@
+﻿// ValheimPlus
+
+using System;
+using System.Linq;
+using System.Reflection;
+using UnityEngine;
+using ValheimPlus.Configurations;
+
+namespace ValheimPlus.ConsoleCommands
+{
+    public class SetConfigurationValue : BaseConsoleCommand
+    {
+        public SetConfigurationValue()
+        {
+            CommandName = "SetValue";
+            HelpText = "Set configuration values - SetValue";
+            // TODO: Get all section names (syncables) and show list of options
+            AdminOnly = true;
+        }
+
+
+        public override bool ParseCommand(string input)
+        {
+            string[] split = input.Split(' ');
+            if (split.Length != 3)
+            {
+                // TODO: Write error message to console
+                // Explanation   Setvalue Section.Property value
+                return false;
+            }
+
+            string sectionAndPropertyName = split[1];
+            string[] split2 = sectionAndPropertyName.Split('.');
+            if (split2.Length != 2)
+            {
+                // Also write error message to console again
+                return false;
+            }
+
+            PropertyInfo sectionProperty = typeof(Configuration).GetProperties(BindingFlags.Instance | BindingFlags.Public)
+                .FirstOrDefault(x => String.Equals(x.Name, split2[0], StringComparison.CurrentCultureIgnoreCase));
+            if (sectionProperty == null)
+            {
+                // TODO: Write error message that the section does not exist
+                return false;
+            }
+
+            var section = sectionProperty.GetValue(Configuration.Current, null);
+
+            PropertyInfo valueProperty = sectionProperty.PropertyType.GetProperties(BindingFlags.Instance | BindingFlags.Public)
+                .FirstOrDefault(x => String.Equals(x.Name, split2[1], StringComparison.CurrentCultureIgnoreCase));
+
+            if (valueProperty == null)
+            {
+                // TODO: Errormessage value does not exist
+                return false;
+            }
+
+            // Switch for value type
+            
+            if (valueProperty.PropertyType == typeof(float))
+            {
+                float newValue = BaseConsoleCommand.GetFloat(split[2]);
+                float oldValue = (float) valueProperty.GetValue(section, null);
+                // TODO: Write old -> new
+                valueProperty.SetValue(section, newValue, null);
+                // TODO: notify for resync
+
+                return true;
+            }
+            if (valueProperty.PropertyType == typeof(int))
+            {
+                int newValue = BaseConsoleCommand.GetInt(split[2]);
+                int oldvalue = (int) valueProperty.GetValue(section, null);
+                // TODO: Write old -> new
+                valueProperty.SetValue(section, newValue, null);
+                // TODO: notify for resync
+                
+                return true;
+            }
+            if (valueProperty.PropertyType == typeof(KeyCode))
+            {
+                KeyCode newValue = BaseConsoleCommand.GetKeyCode(split[2]);
+                KeyCode oldValue = (KeyCode)valueProperty.GetValue(section, null);
+                // TODO: Write old -> new
+                valueProperty.SetValue(section, newValue, null);
+                // TODO: notify for resync
+                
+                return true;
+            }
+
+            // If it got here, we done something weird in the configuration files
+            // All types should be int,float or KeyCode
+
+            return false;
+        }
+    }
+}
