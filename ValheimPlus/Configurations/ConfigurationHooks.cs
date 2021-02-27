b45b436890c071bb99e237f4c89e92e281e3e502
Algorithman <algorithman.chris@googlemail.com>
2021-02-27 10:45:47 +0100

    Save configuration on server if save command is issued by admin

---
+++ ValheimPlus/Configurations/ConfigurationHooks.cs
@@ -0,0 +1,17 @@
+﻿// ValheimPlus
+
+using HarmonyLib;
+
+namespace ValheimPlus.Configurations
+{
+    [HarmonyPatch(typeof(ZNet), "RPC_Save")]
+    public static class ConfigurationHooks
+    {
+        public static void Postfix()
+        {
+            // Just save configuration after a save command is issued
+            // Server side only
+            Configuration.Current.SaveConfiguration();
+        }
+    }
+}
