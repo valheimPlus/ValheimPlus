5f3c17edb486078ccb98dcb866795757c9a8e34f
Zogniton <Zogniton@gmail.com>
2021-02-27 09:49:13 +0200

    Move constants to class

--- ValheimPlus/Inventory.cs
+++ ValheimPlus/Inventory.cs
@@ -41,41 +41,39 @@ namespace ValheimPlus
     [HarmonyPatch(new Type[] { typeof(string), typeof(Sprite), typeof(int), typeof(int) })]
     public static class changeInventorySlotCount
     {
+        private const int playerInventoryMaxRows = 20;
+        private const int playerInventoryMinRows = 4;
+
+        private const int woodChestInventoryMaxRows = 10;
+        private const int woodChestInventoryMinRows = 2;
+        private const int woodChestInventoryMaxCol = 8;
+        private const int woodChestInventoryMinCol = 5;
+
+        private const int ironChestInventoryMaxRows = 20;
+        private const int ironChestInventoryMinRows = 4;
+        private const int ironChestInventoryMaxCol = 20;
+        private const int ironChestInventoryMinCol = 4;
+
         static void Prefix(ref string name, ref Sprite bkg, ref int w, ref int h)
         {
             if (Configuration.Current.Inventory.IsEnabled)
             {
-                // Player inventory number of rows, clamp value in range 4-20
-                int playerInventoryRows = Math.Min(20, Math.Max(4, Configuration.Current.Inventory.playerInventoryRows));
-
-                // Wood chest number of columns, clamp value in range 5-8
-                int woodChestColumns = Math.Min(8, Math.Max(5, Configuration.Current.Inventory.woodChestColumns));
-
-                // Wood chest number of rows, clamp value in range 2-10
-                int woodChestRows = Math.Min(10, Math.Max(2, Configuration.Current.Inventory.woodChestRows));
-
-                // Iron chest number of columns, clamp value in range 6-8
-                int ironChestColumns = Math.Min(8, Math.Max(6, Configuration.Current.Inventory.ironChestColumns));
-
-                // Iron chest number of rows, clamp value in range 3-20
-                int ironChestRows = Math.Min(20, Math.Max(3, Configuration.Current.Inventory.ironChestRows));
-
                 // Wood chest
                 if (h == 2 && w == 5)
                 {
-                    w = woodChestColumns;
-                    h = woodChestRows;
+                    w = Math.Min(woodChestInventoryMaxCol, Math.Max(woodChestInventoryMinCol, Configuration.Current.Inventory.woodChestColumns));
+                    h = Math.Min(woodChestInventoryMaxRows, Math.Max(woodChestInventoryMinRows, Configuration.Current.Inventory.woodChestRows));
                 }
                 // Player inventory
                 else if (h == 4 && w == 8)
                 {
-                    h = playerInventoryRows;
+                    h = Math.Min(playerInventoryMaxRows, Math.Max(playerInventoryMinRows, Configuration.Current.Inventory.playerInventoryRows));
                 }
                 // Iron chest, cart, boat
                 else if (h == 3 && w == 6)
                 {
-                    w = ironChestColumns;
-                    h = ironChestRows;
+                    w = Math.Min(ironChestInventoryMaxCol, Math.Max(ironChestInventoryMinCol, Configuration.Current.Inventory.ironChestColumns));
+                    h = Math.Min(ironChestInventoryMaxRows, Math.Max(ironChestInventoryMinRows, Configuration.Current.Inventory.ironChestRows));
                 }
             }
         }
@@ -84,20 +82,20 @@ namespace ValheimPlus
     [HarmonyPatch(typeof(InventoryGui), "Show")]
     public class changeInventorySize
     {
+        private const float oneRowSize = 70.5f;
+        private const float containerOriginalY = -90.0f;
+        private const float containerHeight = -340.0f;
+
         public static void Postfix(ref InventoryGui __instance)
         {
             if (Configuration.Current.Inventory.IsEnabled)
             {
                 RectTransform container = __instance.m_container;
                 RectTransform player = __instance.m_player;
+                GameObject playerGrid = InventoryGui.instance.m_playerGrid.gameObject;
 
                 // Player inventory background size, only enlarge it up to 6x8 rows, after that use the scroll bar
                 int playerInventoryBackgroundSize = Math.Min(6, Math.Max(4, Configuration.Current.Inventory.playerInventoryRows));
-
-                float oneRowSize = 70.5f;
-                float containerOriginalY = -90.0f;
-                float containerHeight = -340.0f;
-
                 float containerNewY = containerOriginalY - oneRowSize * playerInventoryBackgroundSize;
                 // Resize player inventory
                 player.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, playerInventoryBackgroundSize * oneRowSize);
@@ -105,7 +103,6 @@ namespace ValheimPlus
                 container.offsetMax = new Vector2(610, containerNewY);
                 container.offsetMin = new Vector2(40, containerNewY + containerHeight);
 
-                GameObject playerGrid = InventoryGui.instance.m_playerGrid.gameObject;
                 // Add player inventory scroll bar if it does not exist
                 if (!playerGrid.GetComponent<InventoryGrid>().m_scrollbar)
                 {
@@ -124,7 +121,6 @@ namespace ValheimPlus
                     playerScrollRect.scrollSensitivity = 40;
                     playerScrollRect.inertia = false;
                     playerScrollRect.verticalScrollbarVisibility = ScrollRect.ScrollbarVisibility.AutoHide;
-
                 }
             }
         }
