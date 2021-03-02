09a737d32b18cfa7dacb17f2f440b0e6ac7359a0
Greg Gage <ggage@zorustech.com>
2021-03-02 10:41:48 -0500

    Fix for timemanipulation call

--- ValheimPlus/RPC/VPlusConfigSync.cs
+++ ValheimPlus/RPC/VPlusConfigSync.cs
@@ -76,7 +76,7 @@ namespace ValheimPlus.RPC
                             Configuration.Current = ConfigurationExtra.LoadFromIni(memStream);
 
                             // Needed to make sure client is using server configuration as dayLength is setup before
-                            TimeManipulation.SetupDayLength();
+                            GameClasses.TimeManipulation.SetupDayLength();
 
                             ZLog.Log("Successfully synced VPlus configuration from server.");
                         }
