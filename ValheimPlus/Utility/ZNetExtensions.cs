6dabc74b8629d8d3518907caf3bd721a68b1dcdd
Jules <github@sirskunkalot.de>
2021-03-03 03:11:57 +0100

    add ZNet extensions for instance scope determination

---
+++ ValheimPlus/Utility/ZNetExtensions.cs
@@ -0,0 +1,33 @@
+﻿public static class ZNetExtensions
+{
+    public static bool IsLocalInstance(this ZNet znet)
+    {
+        return !znet.IsDedicated() && znet.IsServer() && (znet.GetNrOfPlayers() > znet.GetPeers().Count);
+    }
+
+    public static bool IsClientInstance(this ZNet znet)
+    {
+        return !znet.IsDedicated() && !znet.IsServer();
+    }
+
+    public static bool IsServerInstance(this ZNet znet)
+    {
+        return !znet.IsLocalInstance() && !znet.IsClientInstance();
+    }
+
+    public static string GetInstanceType(this ZNet znet)
+    {
+        if (znet.IsLocalInstance())
+        {
+            return "Local";
+        } 
+        else if (znet.IsClientInstance())
+        {
+            return "Client";
+        }
+        else
+        {
+            return "Server";
+        }
+    }
+}
