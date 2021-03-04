e94e5bd44ae46d895c3734c5ab53267003a5f4ea
Kevin nX_ J <contact@n-x.xyz>
2021-03-04 12:49:06 +0100

    New - Added fermenter progress display on hover - Fixed unarmed damage - fixed duplicate function from merge - fixed no invalid placement restriction - added auto deposit - changed assembly version - updated cfg to reflect the changes made

--- ValheimPlus/GameClasses/Player.cs
+++ ValheimPlus/GameClasses/Player.cs
@@ -40,52 +40,7 @@ namespace ValheimPlus
         public static void Dodge(object instance, Vector3 dodgeDir) => throw new NotImplementedException();
     }
 
-    /// <summary>
-    /// Starts ABM if not already started
-    /// </summary>
-    [HarmonyPatch(typeof(Player), "UpdatePlacementGhost")]
-    public static class ModifyPlacingRestrictionOfGhost
-    {
-        private static bool Prefix(Player __instance, bool flashGuardStone)
-        {
-            if (Configuration.Current.AdvancedBuildingMode.IsEnabled)
-            {
-                ABM.PlayerInstance = __instance;
-                ABM.run();
-            }
-
-            if (ABM.isActive)
-                return false;
-
-            return true;
-        }
-
-        private static void Postfix(ref Player __instance)
-        {
-            if (ABM.exitOnNextIteration)
-            {
-                try
-                {
-                    if (__instance.m_placementMarkerInstance)
-                    {
-                        __instance.m_placementMarkerInstance.SetActive(false);
-                    }
-                }
-                catch
-                {
-                }
-            }
-
-            if (Configuration.Current.Building.IsEnabled && Configuration.Current.Building.noInvalidPlacementRestriction)
-            {
-                if (__instance.m_placementStatus == Player.PlacementStatus.Invalid)
-                {
-                    __instance.m_placementStatus = Player.PlacementStatus.Valid;
-                    __instance.m_placementGhost.GetComponent<Piece>().SetInvalidPlacementHeightlight(false);
-                }
-            }
-        }
-    }
+   
 
     /// <summary>
     /// Update maximum carry weight based on baseMaximumWeight and baseMegingjordBuff configurations.
@@ -420,14 +375,24 @@ namespace ValheimPlus
 
                 }
             }
-            if (Configuration.Current.AdvancedBuildingMode.IsEnabled && Configuration.Current.Building.noInvalidPlacementRestriction)
+
+            if (Configuration.Current.Building.noInvalidPlacementRestriction)
             {
-                if (__instance.m_placementStatus == Player.PlacementStatus.Invalid)
+                try
                 {
-                    __instance.m_placementStatus = Player.PlacementStatus.Valid;
-                    __instance.m_placementGhost.GetComponent<Piece>().SetInvalidPlacementHeightlight(false);
+                    if (__instance.m_placementStatus == Player.PlacementStatus.Invalid)
+                    {
+                        __instance.m_placementStatus = Player.PlacementStatus.Valid;
+                        __instance.m_placementGhost.GetComponent<Piece>().SetInvalidPlacementHeightlight(false);
+                    }
+                }
+                catch(Exception e)
+                {
+
                 }
             }
+
+
         }
 
 
