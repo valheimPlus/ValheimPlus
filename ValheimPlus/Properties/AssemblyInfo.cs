0d969eb97b986e6a714db02bd8d1fcbf2ef71180
Greg Gage <ggage@zorustech.com>
2021-03-04 12:04:33 -0500

    Sync versions to a const in ValheimPlus.cs (formerly Initialize.cs) to AssemblyInfo and BepInEx annotation

--- ValheimPlus/Properties/AssemblyInfo.cs
+++ ValheimPlus/Properties/AssemblyInfo.cs
@@ -1,5 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
+using ValheimPlus;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
@@ -31,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.9.3.0")]
-[assembly: AssemblyFileVersion("0.9.3.0")]
+[assembly: AssemblyVersion(ValheimPlusPlugin.version)]
+[assembly: AssemblyFileVersion(ValheimPlusPlugin.version)]
