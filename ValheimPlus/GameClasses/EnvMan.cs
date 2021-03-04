b4b3d7ce584617f24a9e605c50b11a779e31f135
Edouard Durand <edouard.durand@gmail.com>
2021-03-05 00:28:19 +0800

    clean brackets + adjust Postfix to be inline with harmonyX (__result)

--- ValheimPlus/GameClasses/EnvMan.cs
+++ ValheimPlus/GameClasses/EnvMan.cs
@@ -9,17 +9,12 @@ namespace ValheimPlus
         /// <summary>
         /// Alter the total day length in seconds base on configuration file
         /// </summary>
-        public static void SetupDayLength()
-        {
-            if (Configuration.Current.Time.IsEnabled)
-            {
+        public static void SetupDayLength() {
+            if (Configuration.Current.Time.IsEnabled) {
                 EnvMan instance = EnvMan.m_instance;
-                if (instance)
-                {
+                if (instance) {
                     instance.m_dayLengthSec = (long)Configuration.Current.Time.totalDayTimeInSeconds;
-                }
-                else
-                {
+                } else {
                     Debug.LogWarning("EnvMan instance not loaded");
                 }
             }
@@ -32,8 +27,7 @@ namespace ValheimPlus
         /// <summary>
         /// Return progression of the day on a range of 0 to 1
         /// </summary>
-        public static double GetDayProgression()
-        {
+        public static double GetDayProgression() {
             double timeSecondsFromStart = ZNet.instance.GetTimeSeconds();
             double timeSecondsFromDay = timeSecondsFromStart % Configuration.Current.Time.totalDayTimeInSeconds;
             return timeSecondsFromDay / Configuration.Current.Time.totalDayTimeInSeconds;
@@ -43,53 +37,39 @@ namespace ValheimPlus
         /// Hook on EnvMan init to alter total day length
         /// </summary>
         [HarmonyPatch(typeof(EnvMan), "Awake")]
-        public static class EnvMan_Awake_Patch
-        {
-            private static void Prefix(ref EnvMan __instance)
-            {
+        public static class EnvMan_Awake_Patch {
+            private static void Prefix(ref EnvMan __instance) {
                 SetupDayLength();
             }
         }
 
         [HarmonyPatch(typeof(EnvMan), "IsDay")]
-        class EnvMan_IsDay_Patch
-        {
-            static bool Postfix(bool result)
-            {
-                if (ZNet.instance.IsServer())
-                {
+        class EnvMan_IsDay_Patch {
+            static void Postfix(ref bool __result) {
+                if (ZNet.instance.IsServer()) {
                     double progression = GetDayProgression();
-                    result = progression >= startOfADay && progression <= startOfNight;
+                    __result = progression >= startOfADay && progression <= startOfNight;
                 }
-                return result;
             }
         }
 
         [HarmonyPatch(typeof(EnvMan), "IsNight")]
-        class EnvMan_IsNight_Patch
-        {
-            static bool Postfix(bool result)
-            {
-                if (ZNet.instance.IsServer())
-                {
+        class EnvMan_IsNight_Patch {
+            static void Postfix(ref bool __result) {
+                if (ZNet.instance.IsServer()) {
                     double progression = GetDayProgression();
-                    result = progression <= startOfADay || progression >= startOfNight;
+                    __result = progression <= startOfADay || progression >= startOfNight;
                 }
-                return result;
             }
         }
 
         [HarmonyPatch(typeof(EnvMan), "IsAfternoon")]
-        class EnvMan_IsAfternoon_Patch
-        {
-            static bool Postfix(bool result)
-            {
-                if (ZNet.instance.IsServer())
-                {
+        class EnvMan_IsAfternoon_Patch {
+            static void Postfix(ref bool __result) {
+                if (ZNet.instance.IsServer()) {
                     double progression = GetDayProgression();
-                    result = progression >= startOfAfternoon && progression <= startOfNight;
+                    __result = progression >= startOfAfternoon && progression <= startOfNight;
                 }
-                return result;
             }
         }
 
@@ -97,16 +77,12 @@ namespace ValheimPlus
         /// Hook on EnvMan to alter night speed
         /// </summary>
         [HarmonyPatch(typeof(EnvMan), "FixedUpdate")]
-        public static class EnvMan_FixedUpdate_Patch
-        {
-            private static void Prefix(ref EnvMan __instance)
-            {
-                if (!Configuration.Current.Time.IsEnabled)
-                {
+        public static class EnvMan_FixedUpdate_Patch {
+            private static void Prefix(ref EnvMan __instance) {
+                if (!Configuration.Current.Time.IsEnabled) {
                     return;
                 }
-                if (ZNet.instance.IsServer() && ZNet.instance.GetNrOfPlayers() > 0 && !__instance.IsTimeSkipping() && __instance.IsNight())
-                {
+                if (ZNet.instance.IsServer() && ZNet.instance.GetNrOfPlayers() > 0 && !__instance.IsTimeSkipping() && __instance.IsNight()) {
                     double timeSeconds = ZNet.instance.GetTimeSeconds();
                     double num = timeSeconds + Helper.applyModifierValue(Time.fixedDeltaTime, Configuration.Current.Time.nightTimeSpeedMultiplier);
                     ZNet.instance.SetNetTime(num);
