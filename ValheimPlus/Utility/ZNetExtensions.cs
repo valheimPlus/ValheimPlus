0cc18ef4028da7dcba86dd1d54dea26c3fad7033
Jules <github@sirskunkalot.de>
2021-03-08 19:12:55 +0100

    simplifying ZNetExtensions (#247)

--- ValheimPlus/Utility/ZNetExtensions.cs
+++ ValheimPlus/Utility/ZNetExtensions.cs
@@ -6,20 +6,20 @@
         Client,
         Server
     }
-     
+
     public static bool IsLocalInstance(this ZNet znet)
     {
-        return !znet.IsDedicated() && znet.IsServer() && (znet.GetNrOfPlayers() > znet.GetPeers().Count);
+        return znet.IsServer() && !znet.IsDedicated();
     }
 
     public static bool IsClientInstance(this ZNet znet)
     {
-        return !znet.IsDedicated() && !znet.IsServer();
+        return !znet.IsServer() && !znet.IsDedicated();
     }
 
     public static bool IsServerInstance(this ZNet znet)
     {
-        return !znet.IsLocalInstance() && !znet.IsClientInstance();
+        return znet.IsServer() && znet.IsDedicated();
     }
 
     public static ZNetInstanceType GetInstanceType(this ZNet znet)
@@ -27,7 +27,7 @@
         if (znet.IsLocalInstance())
         {
             return ZNetInstanceType.Local;
-        } 
+        }
         else if (znet.IsClientInstance())
         {
             return ZNetInstanceType.Client;
