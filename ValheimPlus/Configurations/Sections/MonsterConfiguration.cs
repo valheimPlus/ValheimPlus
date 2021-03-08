a46b63dc974d5a04259312d3d11f9b200a269b8c
Heber Holanda <heberpcl@gmail.com>
2021-03-07 21:52:28 -0300

    monster loot time appear

---
+++ ValheimPlus/Configurations/Sections/MonsterConfiguration.cs
@@ -0,0 +1,7 @@
+﻿namespace ValheimPlus.Configurations.Sections
+{
+    public class MonsterConfiguration : BaseConfig<MonsterConfiguration>
+    {
+        public float dropDelay { get; internal set; } = 0.01f;
+    }
+}
---
+++ ValheimPlus/GameClasses/Monster.cs
@@ -0,0 +1,18 @@
+﻿using BepInEx;
+using HarmonyLib;
+using ValheimPlus.Configurations;
+
+namespace ValheimPlus.GameClasses
+{
+    public class BepInExPlugin : BaseUnityPlugin
+    {
+        [HarmonyPatch(typeof(Ragdoll), "Awake")]
+        static class Ragdoll_Awake_Patch
+        {
+            static void Prefix(Ragdoll __instance)
+            {
+                __instance.m_ttl = Configuration.Current.Monster.dropDelay;
+            }
+        }
+    }
+}
