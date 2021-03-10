636526f9ecf7d04c6992b27c32b8678f1ff80664
David Martel <davidkmartel@gmail.com>
2021-03-10 12:45:11 -0800

    Add config option for Inventory.MoveAll fix.

--- ValheimPlus/Configurations/Sections/InventoryConfiguration.cs
+++ ValheimPlus/Configurations/Sections/InventoryConfiguration.cs
@@ -3,6 +3,7 @@ namespace ValheimPlus.Configurations.Sections
     public class InventoryConfiguration : ServerSyncConfig<InventoryConfiguration>
     {
         public bool inventoryFillTopToBottom { get; set; } = false;
+        public bool mergeWithExistingStacks { get; set; } = false;
         public int playerInventoryRows { get; set; } = 4;
         public int woodChestColumns { get; set; } = 5;
         public int woodChestRows { get; set; } = 2;
--- ValheimPlus/GameClasses/Inventory.cs
+++ ValheimPlus/GameClasses/Inventory.cs
@@ -85,29 +85,32 @@ namespace ValheimPlus
 	/// When merging another inventory, try to merge items with existing stacks.
 	/// </summary>
 	[HarmonyPatch(typeof(Inventory), "MoveAll")]
-	public static class mergeFirstMoveAll
+	public static class Inventory_MoveAll_Patch
 	{
 		private static void Prefix(ref Inventory __instance, ref Inventory fromInventory)
 		{
-			List<ItemDrop.ItemData> list = new List<ItemDrop.ItemData>(fromInventory.GetAllItems());
-			foreach (ItemDrop.ItemData otherItem in list)
+			if (Configuration.Current.Inventory.IsEnabled && Configuration.Current.Inventory.mergeWithExistingStacks)
 			{
-				if (otherItem.m_shared.m_maxStackSize > 1)
+				List<ItemDrop.ItemData> list = new List<ItemDrop.ItemData>(fromInventory.GetAllItems());
+				foreach (ItemDrop.ItemData otherItem in list)
 				{
-					foreach (ItemDrop.ItemData myItem in __instance.m_inventory)
+					if (otherItem.m_shared.m_maxStackSize > 1)
 					{
-						if (myItem.m_shared.m_name == otherItem.m_shared.m_name && myItem.m_quality == otherItem.m_quality)
+						foreach (ItemDrop.ItemData myItem in __instance.m_inventory)
 						{
-							int itemsToMove = Math.Min(myItem.m_shared.m_maxStackSize - myItem.m_stack, otherItem.m_stack);
-							myItem.m_stack += itemsToMove;
-							if (otherItem.m_stack == itemsToMove)
+							if (myItem.m_shared.m_name == otherItem.m_shared.m_name && myItem.m_quality == otherItem.m_quality)
 							{
-								fromInventory.RemoveItem(otherItem);
-								break;
-							}
-							else
-							{
-								otherItem.m_stack -= itemsToMove;
+								int itemsToMove = Math.Min(myItem.m_shared.m_maxStackSize - myItem.m_stack, otherItem.m_stack);
+								myItem.m_stack += itemsToMove;
+								if (otherItem.m_stack == itemsToMove)
+								{
+									fromInventory.RemoveItem(otherItem);
+									break;
+								}
+								else
+								{
+									otherItem.m_stack -= itemsToMove;
+								}
 							}
 						}
 					}
