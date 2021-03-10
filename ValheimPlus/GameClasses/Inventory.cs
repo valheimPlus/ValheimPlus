8358f7a71c75bfda8d68abe87717f1052e0c0ecd
David Martel <davidkmartel@gmail.com>
2021-03-10 09:40:48 -0800

    Extend Inventory.MoveAll to merge with existing stacks first.

--- ValheimPlus/GameClasses/Inventory.cs
+++ ValheimPlus/GameClasses/Inventory.cs
@@ -80,4 +80,39 @@ namespace ValheimPlus
 			}
 		}
 	}
+
+	/// <summary>
+	/// When merging another inventory, try to merge items with existing stacks.
+	/// </summary>
+	[HarmonyPatch(typeof(Inventory), "MoveAll")]
+	public static class mergeFirstMoveAll
+	{
+		private static void Prefix(ref Inventory __instance, ref Inventory fromInventory)
+		{
+			List<ItemDrop.ItemData> list = new List<ItemDrop.ItemData>(fromInventory.GetAllItems());
+			foreach (ItemDrop.ItemData otherItem in list)
+			{
+				if (otherItem.m_shared.m_maxStackSize > 1)
+				{
+					foreach (ItemDrop.ItemData myItem in __instance.m_inventory)
+					{
+						if (myItem.m_shared.m_name == otherItem.m_shared.m_name && myItem.m_quality == otherItem.m_quality)
+						{
+							int itemsToMove = Math.Min(myItem.m_shared.m_maxStackSize - myItem.m_stack, otherItem.m_stack);
+							myItem.m_stack += itemsToMove;
+							if (otherItem.m_stack == itemsToMove)
+							{
+								fromInventory.RemoveItem(otherItem);
+								break;
+							}
+							else
+							{
+								otherItem.m_stack -= itemsToMove;
+							}
+						}
+					}
+				}
+			}
+		}
+	}
 }
