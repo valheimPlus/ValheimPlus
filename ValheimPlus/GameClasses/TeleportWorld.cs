64deebb5b897608463dc6d46177806a402a74737
DANTHEEMAN90 <Danny8890@hotmail.co.uk>
2021-03-27 20:38:01 +0000

    Added option to have big Portal Names displayed

--- ValheimPlus/GameClasses/TeleportWorld.cs
+++ ValheimPlus/GameClasses/TeleportWorld.cs
@@ -22,4 +22,47 @@ namespace ValheimPlus
             return true;
         }
     }
+    /// <summary>
+    /// Show's portal names in big text
+    /// </summary>
+    [HarmonyPatch(typeof(TeleportWorld), "GetHoverText")]
+    public static class TeleportWorld_bigPortalText_Patch
+    {
+        private static bool Prefix(ref TeleportWorld __instance, ref string __result)
+        {
+            string portalName = __instance.GetText();
+            string ConnectionStatus = __instance.HaveTarget() ? "$piece_portal_connected" : "$piece_portal_unconnected";
+
+            if (Configuration.Current.Game.IsEnabled && Configuration.Current.Game.bigPortalNames)
+            {
+                __result = Localization.instance.Localize(string.Concat(new string[]
+                    {
+                    "$piece_portal $piece_portal_tag:\"",
+                    portalName,
+                    "\"  [",
+                    ConnectionStatus,
+                    "]\n[<color=yellow><b>$KEY_Use</b></color>] $piece_portal_settag"
+                    }));
+
+                string BigPortalName = Localization.instance.Localize(string.Concat(new string[]
+                    {
+                    "$piece_portal $piece_portal_tag:",
+                    " ",
+                    "[",portalName,"]"
+                    }));
+
+                MessageHud.instance.ShowMessage(MessageHud.MessageType.Center, BigPortalName, 0, null);
+                return true;
+            }
+            __result = Localization.instance.Localize(string.Concat(new string[]
+                    {
+                    "$piece_portal $piece_portal_tag:\"",
+                    portalName,
+                    "\"  [",
+                    ConnectionStatus,
+                    "]\n[<color=yellow><b>$KEY_Use</b></color>] $piece_portal_settag"
+                    }));
+            return false;
+        }
+    }
 }
