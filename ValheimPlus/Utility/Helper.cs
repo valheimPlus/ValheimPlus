8baa1bf68d50e1c0721a630232ebfd5abff5cec0
Kevin nX_ J <contact@n-x.xyz>
2021-03-27 15:07:51 +0100

    Changelog - Changed helper function for appliance of modifiers to be more precise. - Changed the version number to 0.9.7

--- ValheimPlus/Utility/Helper.cs
+++ ValheimPlus/Utility/Helper.cs
@@ -23,9 +23,7 @@ namespace ValheimPlus
          
         public static float applyModifierValue(float targetValue, float value)
         {
-            if (value == 50) value = 51; // Decimal issue
-            if (value == -50) value = -51; // Decimal issue
-
+            
             if (value <= -100)
                 value = -100;
 
