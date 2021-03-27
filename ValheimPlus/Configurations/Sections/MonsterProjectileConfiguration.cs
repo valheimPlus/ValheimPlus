e2e0085f1716d69e4f51425f864e4467dbfdf728
ahnguyen09 <ahnguyen09@gmail.com>
2021-03-27 16:58:53 -0400

    update code to config cs files

--- ValheimPlus/Configurations/Sections/MonsterProjectileConfiguration.cs
+++ ValheimPlus/Configurations/Sections/MonsterProjectileConfiguration.cs
@@ -1,6 +1,9 @@
-﻿namespace ValheimPlus.Configurations
+﻿namespace ValheimPlus.Configurations.Sections
 {
-    public class MonsterProjectileConfiguration
+    public class MonsterProjectileConfiguration : ServerSyncConfig<MonsterProjectileConfiguration>
     {
+        public float monsterMaxChargeVelocityMultiplier { get; internal set; } = 0;
+
+        public float monsterMaxChargeAccuracyMultiplier { get; internal set; } = 0;
     }
 }
--- ValheimPlus/Configurations/Sections/PlayerProjectileConfiguration.cs
+++ ValheimPlus/Configurations/Sections/PlayerProjectileConfiguration.cs
@@ -1,6 +1,13 @@
-﻿namespace ValheimPlus.Configurations
+﻿namespace ValheimPlus.Configurations.Sections
 {
-    public class PlayerProjectileConfiguration
+    public class PlayerProjectileConfiguration : ServerSyncConfig<PlayerProjectileConfiguration>
     {
+        public float playerMinChargeVelocityMultiplier { get; internal set; } = 0;
+        public float playerMaxChargeVelocityMultiplier { get; internal set; } = 0;
+
+        public float playerMinChargeAccuracyMultiplier { get; internal set; } = 0;
+        public float playerMaxChargeAccuracyMultiplier { get; internal set; } = 0;
+
+        public bool enableScaleWithSkillLevel { get; internal set; } = false;
     }
 }
