fcb9e145d8eb14ac95b6b5c4de00d8ec2dad6913
Nick Penney <baconparticles@gmail.com>
2021-04-07 02:33:32 -0400

    Remove unused configuration option

--- ValheimPlus/Configurations/Sections/HudConfiguration.cs
+++ ValheimPlus/Configurations/Sections/HudConfiguration.cs
@@ -4,7 +4,6 @@
     {
         public bool showRequiredItems { get; internal set; } = false;
         public bool experienceGainedNotifications { get; internal set; } = false;
-        public float chatMessageDistance { get; internal set; }
         public bool displayStaminaValue { get; internal set; } = false;
         public bool removeDamageFlash { get; internal set; } = false;
     }
