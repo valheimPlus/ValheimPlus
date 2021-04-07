c84e48a53bdebdf7b6b31546a2f21e11a373fe4f
Mixone-FinallyHere <miguel.terol.93@gmail.com>
2021-04-07 10:26:34 +0200

    Added the V+ Map Pin Editor UI and Shareable Pins code

--- ValheimPlus/GameClasses/Minimap.cs
+++ ValheimPlus/GameClasses/Minimap.cs
@@ -4,8 +4,11 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 using ValheimPlus.Configurations;
 using ValheimPlus.RPC;
+using ValheimPlus.Utility;
 using Random = UnityEngine.Random;
 
 // ToDo add packet system to convey map markers
@@ -70,6 +73,113 @@ namespace ValheimPlus.GameClasses
 
                 //Start map data save timer
                 ValheimPlusPlugin.mapSyncSaveTimer.Start();
+            }
+        }
+    }
+
+    public static class MapPinEditor_Patches
+    {
+        public static GameObject pinEditorPanel;
+        public static AssetBundle mapPinBundle = null;
+        public static Dropdown iconSelected;
+        public static InputField pinName;
+        public static Toggle sharePin;
+        public static Vector3 pinPos;
+
+        [HarmonyPatch(typeof(Minimap), "Awake")]
+        public static class MapPinEditor_Patches_Awake
+        {
+            private static void AddPin(ref Minimap __instance)
+            {
+                Minimap.PinData addedPin = __instance.AddPin(pinPos, (Minimap.PinType)iconSelected.value, pinName.text, true, false);
+                if (Configuration.Current.Map.shareablePins && sharePin.isOn)
+                    VPlusMapPinSync.SendMapPinToServer(addedPin);
+                pinEditorPanel.SetActive(false);
+                __instance.m_wasFocused = false;
+            }
+
+            private static void Postfix(ref Minimap __instance)
+            {
+                if (Configuration.Current.Map.IsEnabled)
+                {
+                    GameObject iconPanelOld = GameObjectAssistant.GetChildComponentByName<Transform>("IconPanel", __instance.m_largeRoot).gameObject;
+                    for (int i = 0; i < 5; i++)
+                    {
+                        GameObjectAssistant.GetChildComponentByName<Transform>("Icon" + i.ToString(), iconPanelOld).gameObject.SetActive(false);
+                    }
+                    GameObjectAssistant.GetChildComponentByName<Transform>("Bkg", iconPanelOld).gameObject.SetActive(false);
+                    iconPanelOld.SetActive(false);
+                    __instance.m_nameInput.gameObject.SetActive(false);
+                    if (mapPinBundle == null)
+                    {
+                        mapPinBundle = AssetBundle.LoadFromStream(EmbeddedAsset.LoadEmbeddedAsset("Assets.Bundles.map-pin-ui"));
+                    }
+                    GameObject pinEditorPanelParent = mapPinBundle.LoadAsset<GameObject>("MapPinEditor");
+                    pinEditorPanel = GameObject.Instantiate(pinEditorPanelParent.transform.GetChild(0).gameObject);
+                    pinEditorPanel.transform.SetParent(__instance.m_largeRoot.transform, false);
+                    pinEditorPanel.SetActive(false);
+
+                    pinName = pinEditorPanel.GetComponentInChildren<InputField>();
+                    if (pinName != null)
+                        Debug.Log("Pin Name loaded properly");
+                    Minimap theInstance = __instance;
+                    GameObjectAssistant.GetChildComponentByName<Transform>("OK", pinEditorPanel).gameObject.GetComponent<Button>().onClick.AddListener(delegate { AddPin(ref theInstance); });
+                    GameObjectAssistant.GetChildComponentByName<Transform>("Cancel", pinEditorPanel).gameObject.GetComponent<Button>().onClick.AddListener(delegate { Minimap.instance.m_wasFocused = false; });
+                    iconSelected = pinEditorPanel.GetComponentInChildren<Dropdown>();
+                    iconSelected.options.Clear();
+                    int ind = 0;
+                    List<string> list = new List<string> { "Fire", "Home", "Hammer", "Circle", "Rune" };
+                    foreach (string option in list)
+                    {
+                        iconSelected.options.Add(new Dropdown.OptionData(option, Minimap.instance.m_icons[ind].m_icon));
+                        ind++;
+                    }
+                    if (iconSelected != null)
+                        Debug.Log("Dropdown loaded properly");
+                    sharePin = pinEditorPanel.GetComponentInChildren<Toggle>();
+                    if (sharePin != null)
+                        Debug.Log("Share pin loaded properly");
+                    if (!Configuration.Current.Map.shareablePins)
+                        sharePin.gameObject.SetActive(false);
+                }
+            }
+        }
+
+        [HarmonyPatch(typeof(Minimap), "OnMapDblClick")]
+        public static class MapPinEditor_Patches_OnMapDblClick
+        {
+            private static bool Prefix(ref Minimap __instance)
+            {
+                if (Configuration.Current.Map.IsEnabled)
+                {
+                    if (!pinEditorPanel.activeSelf)
+                    {
+                        pinEditorPanel.SetActive(true);
+                    }
+                    if (!pinName.isFocused)
+                    {
+                        EventSystem.current.SetSelectedGameObject(pinName.gameObject);
+                    }
+                    pinPos = __instance.ScreenToWorldPoint(Input.mousePosition);
+                    __instance.m_wasFocused = true;
+                    //iconPanel.transform.localPosition = pinPos;
+                    return false;
+                }
+                return true;
+            }
+        }
+
+        [HarmonyPatch(typeof(Minimap), "UpdateNameInput")]
+        public static class MapPinEditor_Patches_UpdateNameInput
+        {
+            private static bool Prefix(ref Minimap __instance)
+            {
+                if (Configuration.Current.Map.IsEnabled)
+                {
+                    // Break out of this unnecessary thing
+                    return false;
+                }
+                return true;
             }
         }
     }
