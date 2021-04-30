4b0c384bf45171585b63ec1326456a36f349fc08
Lilian Cahuzac <healiha24@gmail.com>
2021-04-30 14:30:17 +0200

    Fix missing check on Fireplace Postfix

--- ValheimPlus/GameClasses/Fireplace.cs
+++ ValheimPlus/GameClasses/Fireplace.cs
@@ -19,7 +19,7 @@ namespace ValheimPlus.GameClasses
             /// </summary>
             private static void Postfix(ref Fireplace __instance)
             {
-                if (!Configuration.Current.FireSource.IsEnabled) return;
+                if (!Configuration.Current.FireSource.IsEnabled || !__instance.m_nview) return;
 
                 if (FireplaceExtensions.IsTorch(__instance.m_name))
                 {
