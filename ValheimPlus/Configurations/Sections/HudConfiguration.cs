9c51e4b9244f9efd0be4f69d3f5baa155078b94f
Kevin nX_ J <contact@n-x.xyz>
2021-09-16 16:13:33 +0200

    Fixes
    - Removed displayStaminaValue setting from Hud Category
    - Removed very old stagnant code of deconstructing items

--- ValheimPlus/Configurations/Sections/HudConfiguration.cs
+++ ValheimPlus/Configurations/Sections/HudConfiguration.cs
@@ -4,7 +4,6 @@
     {
         public bool showRequiredItems { get; internal set; } = false;
         public bool experienceGainedNotifications { get; internal set; } = false;
-        public bool displayStaminaValue { get; internal set; } = false;
         public bool removeDamageFlash { get; internal set; } = false;
         public int displayBowAmmoCounts { get; internal set; } = 0;
     }
