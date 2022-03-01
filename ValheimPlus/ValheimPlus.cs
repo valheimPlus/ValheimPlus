f0ff39cf577c96cc20208bca3ebc10b1a963e5cb
Kevin nX_ J <contact@n-x.xyz>
2022-03-01 15:01:27 +0100

    Updating version number

--- ValheimPlus/ValheimPlus.cs
+++ ValheimPlus/ValheimPlus.cs
@@ -16,7 +16,7 @@ namespace ValheimPlus
     [BepInPlugin("org.bepinex.plugins.valheim_plus", "Valheim Plus", version)]
     public class ValheimPlusPlugin : BaseUnityPlugin
     {
-        public const string version = "0.9.9.7";
+        public const string version = "0.9.9.8";
         public static string newestVersion = "";
         public static bool isUpToDate = false;
 
