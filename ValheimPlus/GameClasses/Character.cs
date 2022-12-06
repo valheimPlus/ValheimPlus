fda59c160e32b0bc9fcb42825aaf83746f7a3838
Lilian Cahuzac <healiha24@gmail.com>
2022-12-06 17:04:16 +0100

    Fix fall damages transpiler

--- ValheimPlus/GameClasses/Character.cs
+++ ValheimPlus/GameClasses/Character.cs
@@ -117,29 +117,28 @@ namespace ValheimPlus.GameClasses
 
             List<CodeInstruction> il = instructions.ToList();
 
+            bool found = false;
+
             // The original code checks if (this.IsPlayer() && num > 4f); if so, it calculates the fall damage as Mathf.Clamp01((num - 4f) / 16f) * 100f
             // ... where num is the fall distance.
             // We want to remove this calculation and replace it with our own, so we replace the calculation with a call to our own method, defined below this one.
             for (int i = 0; i < il.Count; i++)
             {
-                if (il[i].opcode != OpCodes.Newobj) continue;
-                if (i + 11 >= il.Count || il[i + 1].opcode != OpCodes.Stloc_2 || il[i + 11].opcode != OpCodes.Mul)
+                if (!found && il[i].opcode != OpCodes.Newobj)
                 {
-                    // Looks like the wrong location, so we failed and this transpiler needs updating
-                    ZLog.Log("Unable to transpile Character::UpdateGroundContact to patch fall damage calculation");
-                    return instructions;
+                    continue;
                 }
-
-                // OK, looks like we're good! We want to patch a bit further ahead.
-                // il[i] now points to: newobj instance void HitData::.ctor()
-                // We want to keep that and the three instructions after: stloc.2, ldloc.2, ldflda (...)
-                // We want to remove the next 8 instructions (from ldloc.0 to mul, inclusive).
-                il.RemoveRange(i + 4, 8);
-
-                // We now have i+3 = ldflda m_damage, i+4 = stfld m_damage.
-                // We want to insert the call to our damage calculation in between.
-                il.Insert(i + 4, new CodeInstruction(OpCodes.Ldloc_0)); // Load the fall distance
-                il.Insert(i + 5, new CodeInstruction(OpCodes.Call, method_calculateFallDamage));
+                else if (il[i].opcode == OpCodes.Newobj)
+                {
+                    found = true;
+                    continue;
+                }
+                if (il[i].opcode == OpCodes.Ldloc_2)
+                {
+                    il[i].opcode = OpCodes.Ldloc_0;
+                    il.Insert(i + 1, new CodeInstruction(OpCodes.Call, method_calculateFallDamage));
+                }
+                else continue;
 
                 return il.AsEnumerable();
             }
