3f9d35cd9141bd88951098ca8105eafdb1302d1b
Shawn <shawn@wa.ca>
2023-01-23 16:18:32 -0400

    Add configuration and classes for the Eitr Refinery
    Uses the same mechanics as the other "smelters", except with different materials.

--- ValheimPlus/GameClasses/Smelter.cs
+++ ValheimPlus/GameClasses/Smelter.cs
@@ -51,6 +51,12 @@ namespace ValheimPlus.GameClasses
                 __instance.m_maxOre = Configuration.Current.SpinningWheel.maximumFlax;
                 __instance.m_secPerProduct = Configuration.Current.SpinningWheel.productionSpeed;
             }
+            else if (__instance.m_name.Equals(SmelterDefinitions.EitrRefineryName) && Configuration.Current.EitrRefinery.IsEnabled)
+            {
+                __instance.m_maxOre = Configuration.Current.EitrRefinery.maximumSap;
+                __instance.m_maxFuel = Configuration.Current.EitrRefinery.maximumSoftTissue;
+                __instance.m_secPerProduct = Configuration.Current.EitrRefinery.productionSpeed;
+            }
         }
 
     }
@@ -82,7 +88,11 @@ namespace ValheimPlus.GameClasses
             }
             if (__instance.m_name.Equals(SmelterDefinitions.SpinningWheelName) && Configuration.Current.SpinningWheel.IsEnabled && Configuration.Current.SpinningWheel.autoDeposit)
             {
-                return spawn(Helper.Clamp(Configuration.Current.SpinningWheel.autoRange, 1, 50), Configuration.Current.Windmill.ignorePrivateAreaCheck);
+                return spawn(Helper.Clamp(Configuration.Current.SpinningWheel.autoRange, 1, 50), Configuration.Current.SpinningWheel.ignorePrivateAreaCheck);
+            }
+            if (__instance.m_name.Equals(SmelterDefinitions.EitrRefineryName) && Configuration.Current.EitrRefinery.IsEnabled && Configuration.Current.EitrRefinery.autoDeposit)
+            {
+                return spawn(Helper.Clamp(Configuration.Current.EitrRefinery.autoRange, 1, 50), Configuration.Current.EitrRefinery.ignorePrivateAreaCheck);
             }
             bool spawn(float autoDepositRange, bool ignorePrivateAreaCheck)
             {
@@ -195,6 +205,13 @@ namespace ValheimPlus.GameClasses
                     return;
                 autoFuelRange = Configuration.Current.SpinningWheel.autoRange;
                 ignorePrivateAreaCheck = Configuration.Current.SpinningWheel.ignorePrivateAreaCheck;
+            }
+            else if (__instance.m_name.Equals(SmelterDefinitions.EitrRefineryName))
+            {
+                if (!Configuration.Current.EitrRefinery.IsEnabled || !Configuration.Current.EitrRefinery.autoFuel)
+                    return;
+                autoFuelRange = Configuration.Current.EitrRefinery.autoRange;
+                ignorePrivateAreaCheck = Configuration.Current.EitrRefinery.ignorePrivateAreaCheck;
             }
 
             autoFuelRange = Helper.Clamp(autoFuelRange, 1, 50);
@@ -345,6 +362,7 @@ namespace ValheimPlus.GameClasses
         public static readonly string FurnaceName = "$piece_blastfurnace";
         public static readonly string WindmillName = "$piece_windmill";
         public static readonly string SpinningWheelName = "$piece_spinningwheel";
+        public static readonly string EitrRefineryName = "$piece_eitrrefinery";
     }
 
     public static class FurnaceDefinitions
