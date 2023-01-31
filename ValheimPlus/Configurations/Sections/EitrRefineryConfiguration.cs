1f98b2fdb379526e23954f61a1d3fa1cb7023a76
Shawn <shawn@wa.ca>
2023-01-31 19:51:47 -0400

    Nits for the config defaults
    Updating the Eitr Refinery defaults to match others.  Also updating the Spinning Wheel.
    
    Updated the .csproj to build the new file as well.

--- ValheimPlus/Configurations/Sections/EitrRefineryConfiguration.cs
+++ ValheimPlus/Configurations/Sections/EitrRefineryConfiguration.cs
@@ -3,11 +3,17 @@
     public class EitrRefineryConfiguration : ServerSyncConfig<EitrRefineryConfiguration>
     {
         public int maximumSap { get; internal set; } = 20;
+
         public int maximumSoftTissue { get; internal set; } = 20;
+
         public float productionSpeed { get; internal set; } = 40;
-        public bool autoDeposit { get; internal set; } = true;
-        public bool autoFuel { get; internal set; } = true;
+
+        public bool autoDeposit { get; internal set; } = false;
+
+        public bool autoFuel { get; internal set; } = false;
+
         public bool ignorePrivateAreaCheck { get; internal set; } = true;
+
         public float autoRange { get; internal set; } = 10;
     }
 }
--- ValheimPlus/Configurations/Sections/SpinningWheelConfiguration.cs
+++ ValheimPlus/Configurations/Sections/SpinningWheelConfiguration.cs
@@ -4,8 +4,8 @@
     {
         public int maximumFlax { get; internal set; } = 50;
         public float productionSpeed { get; internal set; } = 30;
-        public bool autoDeposit { get; internal set; } = true;
-        public bool autoFuel { get; internal set; } = true;
+        public bool autoDeposit { get; internal set; } = false;
+        public bool autoFuel { get; internal set; } = false;
         public bool ignorePrivateAreaCheck { get; internal set; } = true;
         public float autoRange { get; internal set; } = 10;
     }
