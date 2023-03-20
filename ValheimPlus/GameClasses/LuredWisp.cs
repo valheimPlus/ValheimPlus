854047fee95d3349a5023bc4eb97e25f6b67f668
Jeff Noel <jfnoel@jfnoel.com>
2023-03-20 14:53:53 -0400

    feat: Added WispSpawner configuration (untested)
    Wisp Spawner ("Yagluth Thing") has been added to Valheim Plus configuration.

---
+++ ValheimPlus/GameClasses/LuredWisp.cs
@@ -0,0 +1,31 @@
+﻿using HarmonyLib;
+using System;
+using System.Runtime.Remoting.Messaging;
+using UnityEngine;
+using ValheimPlus.Configurations;
+using ValheimPlus.Utility;
+
+namespace ValheimPlus.GameClasses
+{
+    class LuredWispModification
+    {
+        [HarmonyPatch(typeof(LuredWisp), "UpdateTarget")]
+        public static class ModifyLuredWisp
+        {
+            // "WispSpawner" is from base game
+            private static bool Prefix(ref LuredWisp __instance)
+            {
+                LuredWisp luredWisp = __instance;
+
+
+                if (Configuration.Current.WispSpawner.IsEnabled && !Configuration.Current.WispSpawner.onlySpawnAtNight)
+                {
+                    luredWisp.m_despawnInDaylight = false;
+                }
+
+                return false;
+            }
+
+        }
+    }
+}
---
+++ ValheimPlus/GameClasses/WispSpawner.cs
@@ -0,0 +1,36 @@
+﻿using HarmonyLib;
+using System;
+using System.Runtime.Remoting.Messaging;
+using UnityEngine;
+using ValheimPlus.Configurations;
+using ValheimPlus.Utility;
+
+namespace ValheimPlus.GameClasses
+{
+    class WispSpawnerModification
+    {
+        [HarmonyPatch(typeof(WispSpawner), "TrySpawn")]
+        public static class ModifyWispSpawner
+        {
+            // "WispSpawner" is from base game
+            private static bool Prefix(ref WispSpawner __instance)
+            {
+                WispSpawner wispSpawner = __instance;
+                if (!wispSpawner.m_nview.IsValid() || !wispSpawner.m_nview.IsOwner())
+                {
+                    return false;
+                }
+
+                if (Configuration.Current.WispSpawner.IsEnabled)
+                {
+                    wispSpawner.m_maxSpawned = Configuration.Current.WispSpawner.maximumWisps;
+                    wispSpawner.m_spawnChance = Helper.applyModifierValue(wispSpawner.m_spawnChance, Configuration.Current.WispSpawner.wispSpawnChanceMultiplier);
+                    wispSpawner.m_spawnInterval = Helper.applyModifierValue(wispSpawner.m_spawnInterval, Configuration.Current.WispSpawner.wispSpawnIntervalMultiplier);
+                }
+
+                return true;
+            }
+
+        }
+    }
+}
